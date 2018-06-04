27f8da41cd1c279057374984b05d4b60b8654073
root <root@ns532058.ip-198-245-61.net>
2018-06-04 16:37:28 -0400

    First commit

---
+++ autograbAppend.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -O -L https://data.phishtank.com/data/$APTKEY/online-valid.csv && awk -F',' '{print $2}' online-valid.csv >> fullUri.txt && awk -F',' '{print $2}' online-valid.csv | sed -e 1,1d | cut -d"/" -f3 | sort | uniq >> justDomain.txt
