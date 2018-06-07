5c2f3c73e7c580f77ee6f023f4a73b17f3a835d6
root <root@ns532058.ip-198-245-61.net>
2018-06-07 09:10:16 -0400

    git@github.com:Wheaties466/phishtank-ublock-list.git

--- autograb.sh
+++ autograb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -O -L https://data.phishtank.com/data/$APTKEY/online-valid.csv && awk -F',' '{print $2}' online-valid.csv | sed -e 1,1d > fullUri.txt && awk -F',' '{print $2}' online-valid.csv | sed -e 1,1d | cut -d"/" -f3 | sort | uniq > justDomain.txt
+curl -O -L -o /root/phishtank-ublock-list/online-valid.csv https://data.phishtank.com/data/$APTKEY/online-valid.csv && awk -F',' '{print $2}' online-valid.csv | sed -e 1,1d > fullUri.txt && awk -F',' '{print $2}' online-valid.csv | sed -e 1,1d | cut -d"/" -f3 | sort | uniq > justDomain.txt
