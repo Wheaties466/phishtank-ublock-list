6931392aef62f73537cb088e8cd9ec2487605c04
root <root@ns532058.ip-198-245-61.net>
2018-06-11 10:41:34 -0400

    git@github.com:Wheaties466/phishtank-ublock-list.git

--- autograbAppend.sh
+++ autograbAppend.sh
@@ -2,7 +2,7 @@
 
 echo $APTKEY "This is the API KEY!" >> /var/log/messages
 
-curl -O -L -o /root/phishtank-ublock-list/online-valid.csv https://data.phishtank.com/data/$APTKEY/online-valid.csv
+curl -s -O -L -o /root/phishtank-ublock-list/online-valid.csv https://data.phishtank.com/data/$APTKEY/online-valid.csv
 awk -F',' '{print $2}' /root/phishtank-ublock-list/online-valid.csv | sed -e 1,1d >> /root/phishtank-ublock-list/fullUri.txt
 awk -F',' '{print $2}' /root/phishtank-ublock-list/online-valid.csv | sed -e 1,1d | cut -d"/" -f3 | sort | uniq >> /root/phishtank-ublock-list/justDomain.txt
 
